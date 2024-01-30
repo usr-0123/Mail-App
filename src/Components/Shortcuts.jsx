bc99f67647f8f3c501063da8f7e07c271816df48
usr-0123 <fixedzone365@gmail.com>
2024-01-30 20:41:04 +0300

    Commit changes 4

--- src/Components/Shortcuts.jsx
+++ src/Components/Shortcuts.jsx
@@ -3,23 +3,26 @@ import icon1 from "../assets/Rectangle 556.png";
 import icon2 from "../assets/Rectangle 556 (1).png";
 import icon3 from "../assets/Rectangle 556 (2).png";
 
-function Shortcuts () {
-    return (
-      <>
-        <div className="web-design">
-          <img src={icon1} alt="icon1" />
-          <p>Website Design</p>
-        </div>
-        <div className="Mob-design">
-          <img src={icon2} alt="icon2" />
-          <p>Mobile Design</p>
-        </div>
-        <div className="prod-design">
-          <img src={icon3} alt="icon3" />
-          <p>Product Design</p>
-        </div>
-      </>
-    );
+function Shortcuts() {
+  return (
+    <>
+      <div className="shortcut-title">
+        <p>Shortcuts</p>
+      </div>
+      <div className="web-design">
+        <img src={icon1} alt="icon1" />
+        <p>Website Design</p>
+      </div>
+      <div className="Mob-design">
+        <img src={icon2} alt="icon2" />
+        <p>Mobile Design</p>
+      </div>
+      <div className="prod-design">
+        <img src={icon3} alt="icon3" />
+        <p>Product Design</p>
+      </div>
+    </>
+  );
 }
 
 export default Shortcuts;
